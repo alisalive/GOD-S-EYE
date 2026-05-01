c75cba7fab038a3446228c7b36cc5c2db9ad4e33
alisalive <camalzadss@gmail.com>
2026-05-01 14:06:35 +0400

    chore: remove temp test files

--- test_redirect.py
+++
@@ -1,7 +0,0 @@
-import asyncio 
-import aiohttp 
-async def test(): 
-    async with aiohttp.ClientSession() as s: 
-        r = await s.get('http://millisec.edu.az', allow_redirects=False, timeout=aiohttp.ClientTimeout(total=10), ssl=False) 
-        print('Status:', r.status, 'Location:', r.headers.get('Location')) 
-asyncio.run(test()) 
